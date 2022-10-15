ae6d665b3934132abe70a0324cd7220ee875ff4c
Musa Waghu <mtwaghu@cpp.edu>
2022-10-14 17:17:09 -0700

    wip

--- LoginScreen.py
+++ LoginScreen.py
@@ -3,19 +3,20 @@
 
 import PySimpleGUI as psg
 
-#Our content inside our screen
-screen = [[psg.Text('Username: '), psg.InputText()],
-          [psg.Text('Password:'), psg.InputText()],
-          [psg.Button('Login')]]
+# Our content inside our screen
+screen = [[psg.Text("Username:"), psg.InputText()],
+          [psg.Text("Password:"), psg.InputText()],
+          [psg.Button("Login")]]
 
 # Create the Window
-window = psg.Window('Employee Login', screen)
+window = psg.Window("Employee Login", screen)
 
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
-    if event == psg.WIN_CLOSED: # if user closes window
+    if event == psg.WIN_CLOSED:  # if user closes window, exit out of window
         break
-    print(values[0], " has successfully logged in.")
+    elif event == "Login":
+        psg.popup(values[0], "has successfully logged in.")
 
 window.close()
