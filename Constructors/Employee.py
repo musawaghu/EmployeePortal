da0150f72d1acd70ce1f7f429d9f974dc541cca8
amehr2003 <mehrazarin.a@gmail.com>
2022-10-23 21:13:12 -0700

    added constructors

---
+++ Constructors/Employee.py
@@ -0,0 +1,9 @@
+class Employee:
+    def __init__(self, employee_ID, fullName, email, DOB, phone, address, position):
+        self.employee_ID = employee_ID
+        self.fullName = fullName
+        self.email = email
+        self.DOB = DOB
+        self.phone = phone
+        self.address = address
+        self.position = position
