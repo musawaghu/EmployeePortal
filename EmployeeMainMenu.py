9e761c9a5594aea5365aca7aeba221b19546c208
Musa Waghu <mtwaghu@cpp.edu>
2022-11-20 18:14:56 -0800

    view employee is finished

--- EmployeeMainMenu.py
+++ EmployeeMainMenu.py
@@ -1,25 +1,67 @@
 import PySimpleGUI as psg
-
-from LoginScreen import *
 from CalendarView import *
 from ViewEmployee import *
 
 
 def main():
-    LoginScreen()
-    MainMenuScreen = [[psg.Button("Profile")],
-                      [psg.Button("View Shift")]
+    import requests
+    import json
+    # Creating our login window for user to input
+    LoginScreen = [[psg.Text("First Name:", size=(10, 1), justification="left"), psg.Input()],
+                   [psg.Text("ID:", size=(10, 1), justification="left"), psg.Input()],
+                   [psg.Button("Login", pad=(115, 25))]]
+
+    LoginScreenWindow = psg.Window("Employee Login", LoginScreen, size=(300, 150))
+
+    while True:
+        # reading user's input and saving it to variables
+        event, values = LoginScreenWindow.read()
+        saved_fn = values[0]
+        saved_id = values[1]
+        # if user clicks on the "X" button, loop breaks and window ends
+        if event == psg.WIN_CLOSED:
+            break
+        elif event == "Login":
+            # the following code will validate the input and see if the employee exists in the database. It has some
+            # errors, so its WIP. We call the API, and run a loop to search the employee, and print out if its a
+            # valid entry or not
+            """headers = {
+                "Content-Type": "application/json",
+                "Connection": "keep-alive",
+            }
+            try:
+                request = requests.get("https://uhwxroslh0.execute-api.us-east-1.amazonaws.com/dev/employees")
+                jsonData = request.json()["Items"]
+                for data in jsonData:
+                    if saved_fn == data["firstName"] and (int(saved_id) == data["id"]):
+                        print("if input is correct", saved_id, saved_fn)
+                        psg.popup("has successfully logged in")
+                        break
+                    else:
+                        print("if input is wrong", saved_id, saved_fn)
+                        psg.popup("Incorrect Name and ID")
+                        break
+
+            except requests.exceptions.HTTPError as err:
+                print(err)"""
+            break
+    LoginScreenWindow.close()
+
+    MainMenuScreen = [[psg.Button("Profile", pad=(0, 15))],
+                      [psg.Button("View Shift", pad=(0, 15))]
                       ]
 
-    MenuWindow = psg.Window("Employee Menu", MainMenuScreen)
+    MenuWindow = psg.Window("Employee Menu", MainMenuScreen, size=(300, 150), element_justification="c")
 
     while True:
         event, values = MenuWindow.read()
-        if event == psg.WIN_CLOSED:  # if user closes window, exit out of window
+        if event == psg.WIN_CLOSED:
             break
         elif event == "Profile":
-            ViewEmployee()
+            # Calling ViewEmployee class to show the employee's details, and passing in the variables from above
+            ViewEmployee(saved_fn, saved_id)
         elif event == "View Shift":
+            # calling CalendarView to show employee's shifts and co-workers on that specific shift
             CalendarView()
 
     MenuWindow.close()
--- ViewEmployee.py
+++ ViewEmployee.py
@@ -2,63 +2,92 @@ import PySimpleGUI as psg
 
 
 class ViewEmployee:
-    def __init__(self):
-        screen = [[psg.Text("ID: ")],
-                  [psg.Text("First Name:")],
-                  [psg.Text("Last Name:")],
-                  [psg.Text("Email: ")],
-                  [psg.Text("Date of Birth: ")],
-                  [psg.Text("Phone: ")],
-                  [psg.Text("Address:")],
-                  [psg.Text("Role ID:")],
-                  [psg.Text("Status:")],
-                  [psg.Button("Edit Profile")]
-                  ]
+    def __init__(self, firstName, id):
+        import requests as requests
+        import json
+        self._firstName = firstName
+        self._id = id
 
-        # Create the Window
-        window = psg.Window("Employee Details", screen)
+        headers = {
+            "Content-Type": "application/json",
+            "Connection": "keep-alive",
+        }
+        try:
+            request = requests.get("https://uhwxroslh0.execute-api.us-east-1.amazonaws.com/dev/employees")
+            jsonData = request.json()["Items"]
+            for data in jsonData:
+                if firstName == data["firstName"] and (int(id) == data["id"]):
+                    print(data["id"], data["firstName"], data["lastName"], data["email"])
+                    if data["roleID"] == 1:
+                        data["roleID"] = "Intern"
+                    elif data["roleID"] == 2:
+                        data["roleID"] = "Associate"
+                    elif data["roleID"] == 3:
+                        data["roleID"] = "Supervisor"
+                    elif data["roleID"] == 4:
+                        data["roleID"] = "Manager"
+                    if data["roleID"] == 5:
+                        data["roleID"] = "Executive"
+                    screen = [[psg.Text("ID: "), psg.Text(data["id"])],
+                              [psg.Text("First Name:"), psg.Text(data["firstName"])],
+                              [psg.Text("Last Name:"), psg.Text(data["lastName"])],
+                              [psg.Text("Email: "), psg.Text(data["email"])],
+                              [psg.Text("Date of Birth: "), psg.Text(data["dob"])],
+                              [psg.Text("Phone: "), psg.Text(data["phone"])],
+                              [psg.Text("Address:"), psg.Text(data["address"])],
+                              [psg.Text("Role ID:"), psg.Text(data["roleID"])],
+                              [psg.Text("Status:"), psg.Text(data["status"])],
+                              [psg.Button("Edit Profile")]
+                              ]
+                    # Create the Window
+                    window = psg.Window("Employee Details", screen)
 
-        # Event Loop to process "events" and get the "values" of the inputs
-        while True:
-            event, values = window.read()
-            if event == psg.WIN_CLOSED:  # if user closes window, exit out of window
-                break
-            elif event == "Edit Profile":
-                editScreen = [[psg.Text("ID: ")],
-                              [psg.Text("First Name:"), psg.InputText()],
-                              [psg.Text("Last Name:"), psg.InputText()],
-                              [psg.Text("Email: "), psg.InputText()],
-                              [psg.Text("Date of Birth: "), psg.InputText()],
-                              [psg.Text("Phone: "), psg.InputText()],
-                              [psg.Text("Address:"), psg.InputText()],
-                              [psg.Text("Role ID:")],
-                              [psg.Text("Status:")],
-                              [psg.Button("Save")]]
-                editWindow = psg.Window("Edit Profile", editScreen)
-                while True:
-                    event, values = editWindow.read()
-                    if event == psg.WIN_CLOSED:  # if user closes window, exit out of window
-                        break
-                    elif event == "Save":
-                        import requests
-                        import json
+                    # Event Loop to process "events" and get the "values" of the inputs
+                    while True:
+                        event, values = window.read()
+                        if event == psg.WIN_CLOSED:  # if user closes window, exit out of window
+                            break
+                        elif event == "Edit Profile":
+                            editScreen = [[psg.Text("ID: ")],
+                                          [psg.Text("First Name:"), psg.InputText()],
+                                          [psg.Text("Last Name:"), psg.InputText()],
+                                          [psg.Text("Email: "), psg.InputText()],
+                                          [psg.Text("Date of Birth: "), psg.InputText()],
+                                          [psg.Text("Phone: "), psg.InputText()],
+                                          [psg.Text("Address:"), psg.InputText()],
+                                          [psg.Text("Role ID:")],
+                                          [psg.Text("Status:")],
+                                          [psg.Button("Save")]]
+                            editWindow = psg.Window("Edit Profile", editScreen)
+                            while True:
+                                event, values = editWindow.read()
+                                if event == psg.WIN_CLOSED:  # if user closes window, exit out of window
+                                    break
+                                elif event == "Save":
+                                    import requests
+                                    import json
 
-                        headers = {
-                            "Content-Type": "application/json",
-                            "Connection": "keep-alive",
-                        }
+                                    headers = {
+                                        "Content-Type": "application/json",
+                                        "Connection": "keep-alive",
+                                    }
 
-                        payload = {
-                            "firstName": "Chad",
-                            "roleID": "3"
-                        }
+                                    payload = {
+                                        "firstName": values[0],
+                                        "roleID": "3"
+                                    }
 
-                        try:
-                            r = requests.patch('https://uhwxroslh0.execute-api.us-east-1.amazonaws.com/dev/employee/2',
-                                               data=json.dumps(payload), headers=headers)
-                            print(r.status_code)
-                        except requests.exceptions.HTTPError as err:
-                            print(err)
+                                    try:
+                                        r = requests.patch(
+                                            'https://uhwxroslh0.execute-api.us-east-1.amazonaws.com/dev/employee/2',
+                                            data=json.dumps(payload), headers=headers)
+                                        print(r.status_code)
+                                    except requests.exceptions.HTTPError as err:
+                                        print(err)
 
-                    editWindow.close()
-        window.close()
+                                editWindow.close()
+                    window.close()
+
+        except requests.exceptions.HTTPError as err:
+            print(err)
+
