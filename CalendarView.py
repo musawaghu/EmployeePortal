bb332793aa6d07a5f954d81e626331a1087731d9
Musa Waghu <mtwaghu@cpp.edu>
2022-11-26 17:48:28 -0800

    display shifts finished

--- CalendarView.py
+++ CalendarView.py
@@ -5,7 +5,7 @@ class CalendarView:
     def __init__(self):
         layout = [
             [psg.Text("Date"), psg.InputText(key='Date'),
-             psg.CalendarButton("Select Date", close_when_date_chosen=True, target="Date", format='%Y:%m:%d',
+             psg.CalendarButton("Select Date", close_when_date_chosen=True, target="Date", format='%m/%d/%y',
                                 size=(10, 1))],
             [psg.Submit()]]
 
@@ -16,5 +16,22 @@ class CalendarView:
             if event == psg.WIN_CLOSED:
                 break
             if event == "Submit":
-                psg.popup("This will display which employees have a shift on this day")  # this is a work in progress
+                import requests
+                selectedDate = values['Date']
+                idList = []
+                headers = {
+                    "Content-Type": "application/json",
+                    "Connection": "keep-alive",
+                }
+                try:
+                    request = requests.get("https://uhwxroslh0.execute-api.us-east-1.amazonaws.com/dev/attendance/all")
+                    jsonData = request.json()["Items"]
+                    for data in jsonData:
+                        if selectedDate == data["scheduledDate"]:
+                            idList.append(data["id"])
+                            print(data["id"])
+
+                except requests.exceptions.HTTPError as err:
+                    print(err)
+                psg.popup("This will display which employees have a shift on this day", selectedDate, *idList)
         window.close()
